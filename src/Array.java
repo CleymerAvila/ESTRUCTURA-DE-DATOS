f47b8955d69a7a625bad9a6cbcb5a5e341629149
Cleymer Avila <avilacleymer05@gmail.com>
2025-02-18 09:54:12 -0500

    feat: implement data elimination at first, at last and at a position

--- src/Array.java
+++ src/Array.java
@@ -9,16 +9,19 @@ public class Array {
         count = 0;
     }
 
-    public void insertLast(int data){
+    // insertar al principio
+    public void insertAtLast(int data){
         if (count < size){
             A[count]=data;
             count++;
+            System.out.println("Numero "+ data+ " insertado al final con exito!");
         } else {
             System.out.println("Vector lleno / Error de desbordamiento");
         }
     }
 
-    public void insertFirst(int data){
+    // insertar al final
+    public void insertAtFirst(int data){
         if (count < size){
             if (count > 0){
                 for (int i=count;  i >= 1; i--){
@@ -27,11 +30,13 @@ public class Array {
             }
             A[0] = data;
             count++;
+            System.out.println("Numero "+ data+ " insertado al principio con exito!");
         } else {
             System.out.println("Vector lleno - desbordamiento");
         }
     }
 
+    // insertar en una posicion
     public void insertAtPosition(int data, int position){
         // Validación posicion mayor que tamaño
         if (position > size ){
@@ -49,7 +54,7 @@ public class Array {
             return;
         }
         if (position == 0){
-            insertFirst(data);
+            insertAtFirst(data);
         } else {
             if (count < size){
                 if (count > 0){
@@ -58,6 +63,7 @@ public class Array {
                     }
                     A[position] = data;
                     count++;
+                    System.out.println("Numero "+ data+ " insertado en la posicion " + position + "con exito!");
                 }
             } else {
                 System.out.println("Vector lleno - desbordamiento");
@@ -65,16 +71,65 @@ public class Array {
         }
     }
 
+    // Insertar antes de una posicion
     public void insertBeforePosition(int data, int position){
         position = position -1;
         insertAtPosition(data, position);
     }
 
+    // Insertar después de una posicion
     public void insertAfterPosition(int data, int position){
         position = position + 1;
         insertAtPosition(data, position);
     }
-    public void scrollData(){
+
+    // Eliminar al final
+    public  void deleteAtLast(){
+        if (count == 0){
+            System.out.println("Error , no hay elementos a eliminar!!");
+            return;
+        }
+        count--;
+        System.out.println("Elemento eliminado en la posicion final");
+    }
+
+    // Eliminar al principio
+    public void deleteAtFirst(){
+        if (count == 0){
+            System.out.println("No hay elementos a eliminar");
+            return;
+        }
+        if (count > 0){
+            for (int i=0;  i < count-1; i++){
+                A[i] = A[i + 1];
+            }
+            count--;
+            System.out.println("Elemento eliminado al principio del vector");
+        }
+    }
+
+    // Eliminar en una posicion
+    public void deleteAtPosition(int position){
+        if (position > count){
+            System.out.println("No existe la posicion indicada");
+            return;
+        }
+        if (position > size){
+            System.out.println("La posición indicada excede el tamaño del vector");
+        }
+        if (position == 0){
+            deleteAtFirst();
+        } else {
+            for (int i=position; i < count -1; i++){
+                A[i] = A[i + 1];
+            }
+            count--;
+            System.out.println("Elemento eliminado en la posicion: " + position);
+        }
+    }
+
+    public void printData(){
+        System.out.println("Impresión Elementos");
         for(int i=0; i<count; i++){
             System.out.println("[ "+ A[i]+" ]");
         }
