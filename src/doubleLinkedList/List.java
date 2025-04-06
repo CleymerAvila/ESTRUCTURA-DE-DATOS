a2e3974b576ecef13923d6dc3618e084f7c11b56
Cleymer Avila <avilacleymer05@gmail.com>
2025-04-06 12:23:15 -0500

    feat: implementing double linked list methods for List Class and refactoring existing code to handle previous node of each node on each operation.

--- src/doubleLinkedList/List.java
+++ src/doubleLinkedList/List.java
@@ -43,18 +43,90 @@ public class List {
             this.lastNode = newNode;
         } else {
             this.lastNode.setNext(newNode);
+            newNode.setPrevious(this.lastNode);
             this.lastNode = newNode;
         }
     }
 
     public void insertAfter(Node newNode, String data){
-        Node frog = searchData(data);
-
-        if (frog==null) {
+        Node reference = searchData(data);
+        if (reference==null) {
             System.out.println("El dato no fue encontrado en la lista.");
         } else {
-            newNode.setNext(frog.getNext());
-            frog.setNext(newNode);
+            if (reference==lastNode){
+                insertAtLast(newNode);
+                return;
+            }
+            newNode.setNext(reference.getNext());
+            reference.getNext().setPrevious(newNode);
+            reference.setNext(newNode);
+            newNode.setPrevious(reference);
+        }
+    }
+
+    public void insertBefore(Node newNode, String data){
+        Node reference = searchData(data);
+        if (reference==null){
+            System.out.println("El dato no fue encontrado en la lista");
+        } else {
+            if (reference==firstNode){
+                insertAtFirst(newNode);
+                return;
+            }
+            reference.getPrevious().setNext(newNode);
+            newNode.setNext(reference);
+            newNode.setPrevious(reference.getPrevious());
+            reference.setPrevious(newNode);
+        }
+    }
+
+    public void insertAtPosition(Node newNode, int position){
+        if (position<0){
+            System.out.println("\nNo es posible insertar en esta posicion: ");
+            return;
+        }
+        if (position==0){
+            insertAtFirst(newNode);
+            return;
+        }
+        Node frog = this.firstNode;
+        int pos = 0;
+        while (pos < position && frog.getNext() != null){
+            frog = frog.getNext();
+            pos++;
+        }
+        if (pos != position){
+            System.out.println("\nNo se puede insertar el dato en la posicion indicada");
+        } else {
+            frog.getPrevious().setNext(newNode);
+            newNode.setPrevious(frog.getPrevious());
+            newNode.setNext(frog);
+            frog.setPrevious(newNode);
+        }
+
+    }
+
+    public void deleteByPosition(int position){
+        if (position<0){
+            System.out.println("\nNo es posible eliminar posiciones negativas");
+            return;
+        }
+
+        if (position==0){
+            deleteAtFirst();
+        } else {
+            Node frog = this.firstNode;
+            int pos = 0;
+            while (pos < position && frog.getNext()!=null){
+                frog = frog.getNext();
+                pos++;
+            }
+            if (pos != position){
+                System.out.println("\nNo es posible eliminar la posicion indicada ya que no existe");
+            } else {
+                frog.getPrevious().setNext(frog.getNext());
+                frog.getNext().setPrevious(frog.getPrevious());
+            }
         }
     }
 
@@ -68,8 +140,8 @@ public class List {
                 this.lastNode = null;
             } else {
                 Node aux = this.firstNode;
-
                 this.firstNode = this.firstNode.getNext();
+                this.firstNode.setPrevious(null);
                 aux = null;
             }
         }
@@ -109,10 +181,11 @@ public class List {
                     deleteAtLast();
                 } else {
                     Node frog = this.firstNode;
-                    while (frog.getNext() != lastNode){
+                    while (frog.getNext() != reference){
                         frog = frog.getNext();
                     }
                     frog.setNext(reference.getNext());
+                    reference.getNext().setPrevious(frog);
                     reference  = null;
                 }
             } else {
@@ -132,18 +205,32 @@ public class List {
             Node reference = this.searchData(data);
             if (reference != null){
                 if (reference == firstNode){
-                    deleteAtFirst();
-                } else if (reference == lastNode){
-                    deleteAtLast();
+                    System.out.println("\nNo es posible la operacion. Ya que la referencia es el primer dato de la lista.");
                 } else {
-                    Node frog = this.firstNode;
-                    Node frog2 = this.firstNode;
-                    while (frog2.getNext() != reference){
-                        frog = frog2;
-                        frog2 = frog2.getNext();
-                    }
-                    frog.setNext(reference);
-                    frog2  = null;
+                    reference.getPrevious().getPrevious().setNext(reference);
+                    reference.setPrevious(reference.getPrevious().getPrevious());
+                }
+            } else {
+                System.out.println("No existe el dato referenciado");
+            }
+        }
+    }
+
+    public void deleteAfterReference(String data){
+        if (this.firstNode==null){
+            System.out.println("Lista vacía");
+        } else {
+            // Ojo con las validaciones faltantes
+            // 1. Cuando el nodo referencia es el primero
+            // 2. Cuando el nodo referencia es el ultimo
+            // 3. Cuando el nodo referencia es null (no existe)
+            Node reference = this.searchData(data);
+            if (reference != null){
+                if (reference == lastNode){
+                    System.out.println("\nNo es posible la operacion. Ya que la referencia es el ultimo dato de la lista.");
+                } else {
+                    reference.setNext(reference.getNext().getNext());
+                    reference.getNext().setPrevious(reference);
                 }
             } else {
                 System.out.println("No existe el dato referenciado");
@@ -166,19 +253,23 @@ public class List {
 
     public void traverseList(){
         Node frog = this.firstNode;
-
+        System.out.println();
+        int pos = 0;
         while(frog!=null){
-            System.out.print("<-["+ frog.getData() + "]->");
+            System.out.print("<-[["+ pos++ +"]"+ frog.getData() + "]->");
             frog = frog.getNext();
         }
+        System.out.println();
     }
 
     public void traverseListBackwards(){
         Node frog = this.lastNode;
-
+        System.out.println();
+        int pos = 0;
         while(frog!=null){
-            System.out.print("<-["+ frog.getData() + "]->");
+            System.out.print("<-[["+ pos++ +"]"+ frog.getData() + "]->");
             frog = frog.getPrevious();
         }
+        System.out.println();
     }
 }
